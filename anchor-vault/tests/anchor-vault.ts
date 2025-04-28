562ea9090a3a369b5e146dbb7216b03afcf16012
pipedrimer <pipedrimer@gmail.com>
2025-04-28 11:19:44 -0700

    vault

--- anchor-vault/tests/anchor-vault.ts
+++ anchor-vault/tests/anchor-vault.ts
@@ -1,7 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Keypair, PublicKey } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
 import { AnchorVault } from "../target/types/vault_anchor";
+import { BN } from "bn.js";
 
 describe("anchor-vault", () => {
   // Configure the client to use the local cluster.
@@ -13,6 +14,7 @@ describe("anchor-vault", () => {
 
   const program = anchor.workspace.anchorVault as Program<AnchorVault>;
   const programId = program.programId;
+  
 
   const confirm = async(signature: string): Promise<string> =>{
     const block = await connection.getLatestBlockhash();
@@ -31,12 +33,41 @@ describe("anchor-vault", () => {
 
   const vault_state = PublicKey.findProgramAddressSync([Buffer.from("state"), maker.publicKey.toBuffer()], programId)[0];
 
-  const vault = PublicKey.findProgramAddressSync([Buffer.from("vault"), vault_state.toBuffer()], programId)[0]
+  const vault = PublicKey.findProgramAddressSync([Buffer.from("vault"), vault_state.toBuffer()], programId)[0];
+
+ const accounts= {
+  signer:maker.publicKey,
+  vault_state,
+  vault,
+  systemProgram:SystemProgram.programId,
+ }
  
+ before("airdrop account", async() =>{
+     
+  
+     await connection.requestAirdrop(maker.publicKey, 10*LAMPORTS_PER_SOL).then(confirm)
+ })
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    
+     await program.methods
+    .initialize()
+    .accounts({...accounts})
+    .signers([maker])
+    .rpc()
+    .then(confirm)
+    .then(log)
+    
   });
+
+  it("Deposit", async () =>{
+
+    await program.methods
+    .deposit(new BN(1e9))
+    .accounts({...accounts})
+    .signers([maker])
+    .rpc()
+    .then(confirm)
+    .then(log)
+  })
 });
